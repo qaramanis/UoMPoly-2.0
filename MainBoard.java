ffc766e7abfc90d69badb559f0ca85de08b46e83
Socrates Omiliadis <68393350+socratesomiliadis@users.noreply.github.com>
2023-06-06 12:06:43 +0300

    Gui (#2)
    * Added MainBoard Screen
    Added StartingScreen
    
    * Initialized Starting Screen in Main

--- MainBoard.java
+++ MainBoard.java
@@ -1,6 +1,41 @@
-import java.util.ArrayList;
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Insets;
 
-public class MainBoard {
-    public ArrayList<Player> players = new ArrayList<>();
+public class MainBoard extends JFrame {
+    private JPanel container;
+    private JPanel top;
+    private JPanel middle;
+    private JPanel bottom;
+    private JPanel topLeft;
+    private JPanel topMiddle;
+    private JLabel playerNumber;
+    private JLabel currentBalance;
+    private JPanel topRight;
+    private JButton propertyBtn;
+    private JPanel dice;
+    private JLabel diceNumbers;
+    private JPanel bottomButtons;
+    private JPanel rightButton;
+    private JPanel middleButton;
+    private JPanel LeftButton;
+    private JButton diceBtn;
+    private JPanel tooltips;
+    private JPanel tooltipLeft;
+    private JLabel currentPos;
+    private JPanel tooltipRight;
+    private JLabel tooltipRightLabel;
+
+    public MainBoard(){
+        setContentPane(container);
+        setTitle("UoMPoly");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(1180, 1080);
+        setVisible(true);
+    }
+
 
 }
